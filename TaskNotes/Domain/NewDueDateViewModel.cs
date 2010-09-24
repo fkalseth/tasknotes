f53581212feae8b6a9a75fd7fc5188bb0ba9a064
Fredrik Kalseth <fredrik@kalseth.com>
2010-09-24 22:27:29 +0200

    left a few out..

---
+++ TaskNotes/Domain/NewDueDateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskNotes.Domain
+{
+    public class NewDueDateViewModel
+    {
+        public Guid TaskId { get; set; }
+        
+        public string Description { get; set; }
+        
+        [Required]
+        public DateTime NewDueDate { get; set; }
+    }
+}
---
+++ TaskNotes/Domain/TasksViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskNotes.Domain
+{
+    public class TasksViewModel
+    {
+        private readonly TaskDto[] _tasks;
+
+        public TasksViewModel(TaskDto[] tasks)
+        {
+            _tasks = tasks;
+        }
+
+        public TaskDto[] Tasks { get { return _tasks; } }
+    }
+}
